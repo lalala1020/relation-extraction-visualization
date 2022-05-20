36f9224820fd024550be329943b840f9375aaa4f
lalala1020 <darlwj@163.com>
2022-05-20 09:51:56 +0000

    upload files

--- about.py
+++ about.py
@@ -1,11 +1,12 @@
+import os
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
-
-# UPLOAD_FOLDER = '../tmp'
-# ALLOWED_EXTENSIONS = {'json'}
+UPLOAD_FOLDER = '../upload'
+ALLOWED_EXTENSIONS = {'json'}
 
 app = Flask(__name__)
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -16,6 +17,8 @@ def success():
     if request.method == 'POST':
         f = request.files['file']
         f.save(f.filename)
+        # f.save(app.config['UPLOAD_FOLDER'], f.filename)
+        # # f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
         return render_template('visualization.html')
 
 if __name__ == '__main__':

af3693a87c912aa98f55309766e4e7a44b44b827
lalala1020 <darlwj@163.com>
2022-05-20 12:45:01 +0000

    upload files

--- about.py
+++ about.py
@@ -16,9 +16,12 @@ def index():
 def success():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(f.filename)
-        # f.save(app.config['UPLOAD_FOLDER'], f.filename)
-        # # f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+        # f.save(f.filename)
+        basepath = os.path.dirname(__file__)  # 当前文件所在路径
+        upload_path = os.path.join(basepath, 'save_files',secure_filename(f.filename))
+        # 注意：没有的文件夹一定要先创建，不然会提示没有该路径
+        upload_path = os.path.abspath(upload_path) # 将路径转换为绝对路径
+        f.save(upload_path)
         return render_template('visualization.html')
 
 if __name__ == '__main__':
